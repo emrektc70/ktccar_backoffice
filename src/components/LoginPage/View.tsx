2d5ca510fce4a51469838e575cc38bbdbea0e830
emrektc70 <emrektc70@gmail.com>
2023-07-12 14:44:02 +0200

    save

--- src/components/LoginPage/View.tsx
+++ src/components/LoginPage/View.tsx
@@ -2,7 +2,7 @@ import Separator from "../Separator/view";
 import { useNavigate } from "react-router-dom";
 import TextField from "@mui/material/TextField";
 import styles from "./styles.module.scss";
-
+import porsche3 from './assets/porsche3.jpg';
 
 type Props = {
   loginData: VoidFunction;
@@ -28,31 +28,36 @@ const View: React.FC<Props> = ({
   return (
     <div className={styles.contentLogin}>
       <div className={styles.box}>
-
-        <div className={styles.title}> Se connecter </div>
-        <Separator />
-        <div className={styles.inputBox}>
-          <TextField label="email" variant="filled" className={styles.inputemail} value={email} onChange={handleChangeEmail}
-            style={{ background: "#fff" }} fullWidth
-
-          />
-          <div className={styles.boxPassword}>
-            <TextField label="Mot de passe" variant="filled" className={styles.inputpassword}
-              value={password} onChange={handleChangePassword} style={{ background: "#fff" }} fullWidth type="password" />
-          </div>
-
-          <div className={styles.resetPassword}> Mot de passe oublié</div>
-          <div className={styles.test} >
-            <Separator />
-          </div>
-          <div className={styles.bottomBox}>
-            <div className={styles.register} onClick={navigateRegister}>
-              Créer un compte
+        <div className={styles.left}>
+          <div className={styles.title}> Se connecter </div>
+          <Separator />
+          <div className={styles.inputBox}>
+            <TextField label="email" variant="filled" className={styles.inputemail} value={email} onChange={handleChangeEmail}
+              style={{ background: "#fff" }} fullWidth
+            />
+            <div className={styles.boxPassword}>
+              <TextField label="Mot de passe" variant="filled" className={styles.inputpassword}
+                value={password} onChange={handleChangePassword} style={{ background: "#fff" }} fullWidth type="password" />
             </div>
 
-            <div className={styles.connexion} onClick={loginData}>
-              Se connecter
+            <div className={styles.resetPassword}> Mot de passe oublié</div>
+            <div className={styles.test} >
+              <Separator />
             </div>
+            <div className={styles.bottomBox}>
+              <div className={styles.register} onClick={navigateRegister}>
+                Créer un compte
+              </div>
+
+              <div className={styles.connexion} onClick={loginData}>
+                Se connecter
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.right}>
+          <div>
+            <img src={porsche3} className={styles.imgStyle} />
           </div>
         </div>
       </div>
