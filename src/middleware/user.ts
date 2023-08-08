462b9bfc38fd847f40d7a58a1ad9d13a2b41130a
emrektc70 <emrektc70@gmail.com>
2023-08-08 13:59:15 +0200

    comment

--- src/middleware/user.ts
+++ src/middleware/user.ts
@@ -1,22 +1,26 @@
 import { GET_USER_ID, changeUserFields } from "../actions/user";
 import httpGetUser from "../api/user/httpGetUser";
 
-const userMiddleware =
-    (store: any) => (next: any) => async (action: any) => {
-        switch (action.type) {
-            case GET_USER_ID:
-                try {
-                    const res = await httpGetUser(action.id)
-                    store.dispatch(changeUserFields('firstName', res.firstName))
-                    store.dispatch(changeUserFields('lastName', res.lastName))
-                    store.dispatch(changeUserFields('userName', res.username))
-                } catch (e) {
-                    console.log(e)
-                }
-                break
-            default:
-                next(action);
-        }
-    };
+const userMiddleware = (store: any) => (next: any) => async (action: any) => {
+  switch (action.type) {
+    case GET_USER_ID:
+      try {
+        const res = await httpGetUser(action.id);
+        console.log(res);
+        /*    if (res === undefined) {
+          window.location.href = "/login";
+        } */
+
+        store.dispatch(changeUserFields("firstName", res.firstName));
+        store.dispatch(changeUserFields("lastName", res.lastName));
+        store.dispatch(changeUserFields("userName", res.username));
+      } catch (e) {
+        console.log(e);
+      }
+      break;
+    default:
+      next(action);
+  }
+};
 
 export default userMiddleware;
