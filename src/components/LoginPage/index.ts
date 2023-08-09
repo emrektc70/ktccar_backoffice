0c33673ee6d52cf41146c624552068fd65065dfa
emrektc70 <emrektc70@gmail.com>
2023-08-09 16:00:04 +0200

    save

--- src/components/LoginPage/index.ts
+++ src/components/LoginPage/index.ts
@@ -6,13 +6,14 @@ import {
 } from "../../actions/authentification";
 import { RootState } from "../../store/store";
 import ViewModel from "./Model";
+
 import { changeSecurityFields } from "../../actions/security";
 
 // eslint-disable-next-line no-unused-vars
 const mapStateToProps = (state: RootState) => ({
   password: state.authentication.password,
   email: state.authentication.email,
-  isLog: state.security.isLog
+  isLog: state.security.isLog,
 });
 
 // eslint-disable-next-line no-unused-vars
@@ -25,7 +26,7 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
   },
   changeSecurityFields: (field: string, value: any) => {
     dispatch(changeSecurityFields(field, value));
-  }
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ViewModel);
